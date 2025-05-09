ae756a7bcb01f01f65a7acfd06c4f2fc845ae968
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-05-09 20:35:08 +0300

    refractor: use FieldsetBlock component to render form fieldset; add styles

--- src/app/RegistrationPage.tsx
+++ src/app/RegistrationPage.tsx
@@ -1,78 +1,49 @@
-import CountrySelector from './components/CountrySelector/CountrySelector';
-import InputElement from './components/InputElement/InputElement';
 import { registrationPageData } from './registration-page-data/registration-page-data';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { registrationSchema } from './registration-page-data/registrationSchema';
+import FieldsetBlock from './components/FieldsetBlock/FieldsetBlock';
 
 export default function RegistrationPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(registrationSchema),
+  });
+
+  const onSubmit = void handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
-    <>
-      <h2 className="text-3xl p-4 text-center ">Registration</h2>
-      <form className="flex flex-col gap-3 p-4">
-        {/** user info  */}
-        <fieldset className="flex flex-row gap-6 p-4 flex-wrap">
-          <legend className="text-2xl capitalize">
-            {registrationPageData.userInfo.title}
-          </legend>
-          {registrationPageData.userInfo.content.map((property) => (
-            <InputElement
-              title={property.title}
-              placeholder={property.placeholder}
-              id={property.id}
-              type={property.type}
-              key={property.id}
-            />
-          ))}
-        </fieldset>
-
-        {/** user address  */}
-        <fieldset className="flex flex-row gap-6 p-4 flex-wrap">
-          <legend className="text-2xl capitalize">
-            {registrationPageData.userAddress.title}
-          </legend>
-          {registrationPageData.userAddress.content.map((property) => (
-            <InputElement
-              title={property.title}
-              placeholder={property.placeholder}
-              id={property.id}
-              type={property.type}
-              key={property.id}
-            />
-          ))}
-          <CountrySelector />
-        </fieldset>
-
-        {/** pet information  */}
-        <fieldset className="flex flex-row gap-6 p-4 flex-wrap">
-          <legend className="text-2xl capitalize">
-            {registrationPageData.petInfo.title}
-          </legend>
-          {registrationPageData.petInfo.content.map((property) => (
-            <InputElement
-              title={property.title}
-              placeholder={property.placeholder}
-              id={property.id}
-              type={property.type}
-              key={property.id}
-            />
-          ))}
-        </fieldset>
-
-        {/** email and password  */}
-        <fieldset className="flex flex-row gap-6 p-4 flex-wrap">
-          <legend className="text-2xl capitalize">
-            {registrationPageData.userLogin.title}
-          </legend>
-          {registrationPageData.userLogin.content.map((property) => (
-            <InputElement
-              title={property.title}
-              placeholder={property.placeholder}
-              id={property.id}
-              type={property.type}
-              key={property.id}
-            />
-          ))}
-        </fieldset>
-        <input type="submit" className="w-fit p-4" />
+    <section className="bg-white flex flex-col items-center justify-center">
+      <h2 className="text-3xl p-2 text-center text-[var(--color-jungle)] font-medium">
+        Registration
+      </h2>
+      <form
+        className="flex flex-col gap-2 p-2 items-center"
+        onSubmit={onSubmit}
+      >
+        {registrationPageData.map((registrationFieldset) => (
+          <FieldsetBlock
+            title={registrationFieldset.title}
+            content={registrationFieldset.content}
+            hint={registrationFieldset.hint}
+            register={register}
+            errors={errors}
+            key={registrationFieldset.title
+              .toLocaleLowerCase()
+              .split(' ')
+              .join('-')}
+          />
+        ))}
+        <input
+          type="submit"
+          className="w-fit p-1 pl-3 pr-3 text-white bg-[var(--color-jungle)] rounded-xl m-2 text-2xl capitalize font-medium"
+        />
       </form>
-    </>
+    </section>
   );
 }
