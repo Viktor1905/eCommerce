bfa65b41cb831880a546a483948ffb974ee1a0aa
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-05-16 14:32:26 +0300

    fix: implement navigation with RoutingLink component

--- src/components/RoutingLink/RoutingLink.tsx
+++ src/components/RoutingLink/RoutingLink.tsx
@@ -1,14 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function RoutingLink({
   additionalText,
   linkText,
+  redirectTo,
 }: {
   additionalText: string;
   linkText: string;
+  redirectTo: string;
 }) {
+  const navigate = useNavigate();
+  function onClick() {
+    void navigate('/' + redirectTo);
+  }
   return (
     <div className="flex gap-2 bg-white items-center p-2 justify-center font-additional">
       <span className="text-jungle">{additionalText}</span>
-      <a href="#" className="text-jungle m-2 font-bold  font-main">
+      <a href={redirectTo} onClick={onClick} className="text-jungle m-2 font-bold  font-main">
         {linkText}
       </a>
     </div>
--- src/pages/register/RegistrationPage.tsx
+++ src/pages/register/RegistrationPage.tsx
@@ -4,7 +4,11 @@ import RoutingLink from '../../components/RoutingLink/RoutingLink';
 export default function RegistrationPage() {
   return (
     <section className="w-fit m-auto flex flex-col justify-center items-center rounded-lg bg-white">
-      <RoutingLink additionalText="Already have an account?" linkText="Sign in →" />
+      <RoutingLink
+        additionalText="Already have an account?"
+        linkText="Sign in →"
+        redirectTo="login"
+      />
       <RegistrationForm />
     </section>
   );
