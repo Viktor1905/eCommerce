fd470602b1bb7d612b160627eb4bb72e04a8461d
Viktor1905 <webdevvva@gmail.com>
2025-05-18 14:54:45 +0300

    feat: done history entry login ban

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,16 @@ import { store } from './store/store.ts';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/react-query.ts';
 import './i18n.ts';
+import { BrowserRouter } from 'react-router-dom';
 
 export function renderApp(container: HTMLElement) {
   createRoot(container).render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-          <App />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
         </Provider>
       </QueryClientProvider>
     </StrictMode>
