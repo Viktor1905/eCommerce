37661d636ec7b10602738237842ae491efa3788f
vitali kozhar <vox@mail.com>
2025-05-18 20:21:22 +0400

    feat: protect login and registration routes from access by authenticated users

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,19 @@ import { NotFoundPage } from './pages/pageNotFound/Page404.tsx';
 import { AboutUsPage } from './pages/about/AboutPage.tsx';
 import { ProfilePage } from './pages/profile/ProfilePage.tsx';
 import { TeamPage } from './pages/team/TeamPage.tsx';
+import { Navigate } from 'react-router-dom';
 
 function App() {
+  const authStatus = localStorage.getItem('firstName');
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="login" element={authStatus ? <Navigate to="/" /> : <LoginPage />} />
+        <Route
+          path="/registration"
+          element={authStatus ? <Navigate to="/" /> : <RegistrationPage />}
+        />
         <Route path="catalog" element={<CatalogPage />} />
         {/* <Route path="/product/:id" element={<ProductDetailsPage />} /> */}
         <Route path="cart" element={<CartPage />} />
