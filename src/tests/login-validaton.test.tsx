763479ccad7af80b878e0b501956af6abc9d6a73
Viktor1905 <webdevvva@gmail.com>
2025-05-21 20:55:20 +0300

    fix: tests login page fixed

--- src/tests/login-validaton.test.tsx
+++ src/tests/login-validaton.test.tsx
@@ -3,6 +3,7 @@ import { LoginInput } from '../pages/authorization/components/Login-input.tsx';
 import type { LoginInputProps } from '../pages/authorization/components/Login-input.tsx';
 import { UseFormRegisterReturn } from 'react-hook-form';
 import { LoginForm } from '../pages/authorization/components/Login-form.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 vi.mock('./assets/eye.svg', (): string => 'eye-icon');
 vi.mock('./assets/eye-off.svg', (): string => 'eye-off-icon');
@@ -27,19 +28,37 @@ describe('Login Email input', (): void => {
     expect(screen.getByPlaceholderText('Test placeholder')).toHaveAttribute('type', 'email');
   });
   it('Error: email not contain "@"', async (): Promise<void> => {
-    render(<LoginForm />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input = screen.getByPlaceholderText('Enter you email');
     fireEvent.change(input, { target: { value: 'dfda' } });
     await screen.findByText("Email must contain an '@' symbol");
   });
   it('Error: Email not contain a domain name (e.g., example.com)', async (): Promise<void> => {
-    render(<LoginForm />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input = screen.getByPlaceholderText('Enter you email');
     fireEvent.change(input, { target: { value: 'dfda@' } });
     await screen.findByText('Email must contain a domain name (e.g., example.com)');
   });
   it('Error: Email have leading or trailing spaces', async (): Promise<void> => {
-    render(<LoginForm />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input = screen.getByPlaceholderText('Enter you email');
     fireEvent.change(input, { target: { value: 'dfda@dfo.com ' } });
     await screen.findByText('Email must not have leading or trailing spaces');
@@ -47,47 +66,93 @@ describe('Login Email input', (): void => {
     await screen.findByText('Email must not have leading or trailing spaces');
   });
   it('renders password input with toggle button', (): void => {
-    render(<LoginInput {...baseProps} type="password" label="password" placeholder="password" />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <LoginInput {...baseProps} type="password" label="password" placeholder="password" />
+            }
+          />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input: HTMLElement = screen.getByPlaceholderText('password');
     expect(input).toHaveAttribute('type', 'password');
-    const button: HTMLElement = screen.getByAltText('Toggle password visibility');
+    const button: HTMLElement = screen.getByText(/🙈|🙉/);
     expect(button).toBeInTheDocument();
   });
   it('Error: Password not contains at least one digit', async (): Promise<void> => {
-    render(<LoginForm />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input = screen.getByPlaceholderText('Enter you password');
     fireEvent.change(input, { target: { value: 'dfda' } });
     await screen.findByText('Password must contain at least one digit');
   });
   it('Error: Password must contain at least one uppercase letter', async (): Promise<void> => {
-    render(<LoginForm />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input = screen.getByPlaceholderText('Enter you password');
     fireEvent.change(input, { target: { value: '1dfda' } });
     await screen.findByText('Password must contain at least one uppercase letter');
   });
   it('Error: Password must contain at least one lowercase letter', async (): Promise<void> => {
-    render(<LoginForm />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input = screen.getByPlaceholderText('Enter you password');
     fireEvent.change(input, { target: { value: '1D' } });
     await screen.findByText('Password must contain at least one lowercase letter');
   });
   it('Error: Password must be longer than 8 characters', async (): Promise<void> => {
-    render(<LoginForm />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input = screen.getByPlaceholderText('Enter you password');
     fireEvent.change(input, { target: { value: '1Dds' } });
     await screen.findByText('Password must be longer than 8 characters');
   });
   it('toggles password button', (): void => {
-    render(<LoginInput {...baseProps} type="password" label="password" placeholder="password" />);
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <LoginInput {...baseProps} type="password" label="password" placeholder="password" />
+            }
+          />
+        </Routes>{' '}
+      </BrowserRouter>
+    );
     const input: HTMLElement = screen.getByPlaceholderText('password');
-    const button: HTMLElement = screen.getByAltText('Toggle password visibility');
-    const buttonElem = button as HTMLImageElement;
-    const buttonSrc: string = buttonElem.src;
+    const button: HTMLElement = screen.getByText(/🙈|🙉/);
+    const buttonElem = button;
+    const buttonVisibility = buttonElem.textContent;
     fireEvent.click(button);
     expect(input).toHaveAttribute('type', 'text');
-    expect(buttonElem.src).not.toBe(buttonSrc);
+    expect(button.textContent).not.toBe(buttonVisibility);
     fireEvent.click(button);
     expect(input).toHaveAttribute('type', 'password');
-    expect(buttonElem.src).toBe(buttonSrc);
+    expect(buttonElem.textContent).toBe(buttonVisibility);
   });
 });
