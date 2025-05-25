fb19c82493aaf6079b5c4697c46acd696496562e
Viktor1905 <webdevvva@gmail.com>
2025-05-25 21:20:43 +0300

    feat: start and understood how to make request with filter + start CatalogFilter

--- src/pages/catalog/components/CatalogFilter.tsx
+++ src/pages/catalog/components/CatalogFilter.tsx
@@ -1,15 +1,36 @@
-import { ReactElement } from 'react';
+import { FormEvent, ReactElement } from 'react';
+import { useForm } from 'react-hook-form';
 
 export function CatalogFilter(): ReactElement {
-  const filterList = ['1 filter', '2 filter', '3 filter', '4 filter'];
+  const filterList = ['2 filter', '3 filter', '4 filter'];
+  const { register, handleSubmit } = useForm<Filters>();
+  const onSubmit = (data: Filters) => {
+    console.log(data);
+  };
   return (
-    <section className={'bg-khaki flex flex-col'}>
+    <form
+      className={'bg-khaki flex flex-col'}
+      onSubmit={(event: FormEvent<HTMLFormElement>): void => void handleSubmit(onSubmit)(event)}
+    >
+      <div>
+        <input type="checkbox" id="sale" {...register('discounted')} />
+        <label htmlFor="sale">Sale</label>
+      </div>
       {filterList.map((item, index) => (
         <button key={index} className={'text-nowrap'}>
           {' '}
           {item}
         </button>
       ))}
-    </section>
+      <button type="submit" className={'text-nowrap'}>
+        Submit
+      </button>
+    </form>
   );
+}
+interface Filters {
+  minPrice: number;
+  maxPrice: number;
+  category: string;
+  discounted: boolean;
 }
